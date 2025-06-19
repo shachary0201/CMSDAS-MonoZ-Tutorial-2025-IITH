a44122bbacb1d95fb57464c60d20a4fc73e9580b
shriniketan0201 <62455796+shriniketan0201@users.noreply.github.com>
2025-06-19 19:21:50 +0530

    Update bh_output.py

--- processing/bh_output.py
+++ processing/bh_output.py
@@ -6,14 +6,11 @@ import pickle
 with gzip.open("histograms-reformed.pkl.gz", "rb") as f:
     bh_output = pickle.load(f)
 
-# Print the type of bh_output
 print(type(bh_output))
 
-# Print the keys if it is a dict
 if isinstance(bh_output, dict):
     print("Keys in bh_output:", list(bh_output.keys()))
 
-# Optionally print one histogram or summary
 for key in list(bh_output.keys())[:3]:
     print(f"\nContents of '{key}':")
     print(bh_output[key])
